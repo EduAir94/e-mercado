586dfe7d27923fdc830b8fa2ff81d1346d0b6bba
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2022-08-20 18:48:15 -0300

    feat: :recycle: eslint implementation for fast clean formatting

--- js/login.js
+++ js/login.js
@@ -1,84 +1,74 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const validate_email = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      );
-  };
-
-  const form = document.getElementById("login");
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('login');
   // Loop over them and prevent submission
-  form.addEventListener("submit", (event) => {
+  form.addEventListener('submit', (event) => {
     const data = new FormData(form);
-    const email = data.get("email");
-    form.classList.add("was-validated");
+    const email = data.get('email');
+    form.classList.add('was-validated');
     if (form.checkValidity() === false) {
       if (email) {
-        document.getElementById("email_invalid_feedback").innerHTML =
-          "E-mail no es válido";
+        document.getElementById('email_invalid_feedback').innerHTML = 'E-mail no es válido';
       } else {
-        document.getElementById("email_invalid_feedback").innerHTML =
-          "Ingresa tu e-mail";
+        document.getElementById('email_invalid_feedback').innerHTML = 'Ingresa tu e-mail';
       }
       event.preventDefault();
       event.stopPropagation();
       return;
     }
-    const password = data.get("password");
-    localStorage.setItem("email", email);
+    localStorage.setItem('email', email);
     event.preventDefault();
-    window.location.href = "/market.html";
+    window.location.href = '/market.html';
   });
 
-  const show_hide_pass = document.querySelector(".btn_show_hide_pass");
-  const pass_input = document.getElementById("password");
-  show_hide_pass.addEventListener("click", function (e) {
+  const showHidePass = document.querySelector('.btn_show_hide_pass');
+  const passInput = document.getElementById('password');
+  showHidePass.addEventListener('click', (e) => {
     e.preventDefault();
-    const i = show_hide_pass.querySelector("i");
-    const show = i.classList.contains("bi-eye");
+    const i = showHidePass.querySelector('i');
+    const show = i.classList.contains('bi-eye');
     if (show) {
-      i.classList.remove("bi-eye");
-      i.classList.add("bi-eye-slash");
-      pass_input.type = "text";
+      i.classList.remove('bi-eye');
+      i.classList.add('bi-eye-slash');
+      passInput.type = 'text';
     } else {
-      i.classList.add("bi-eye");
-      i.classList.remove("bi-eye-slash");
-      pass_input.type = "password";
+      i.classList.add('bi-eye');
+      i.classList.remove('bi-eye-slash');
+      passInput.type = 'password';
     }
   });
 });
 
-const client_id = document
-  .querySelector("#g_id_onload")
-  .getAttribute("data-client_id");
+const clientId = document
+  .querySelector('#g_id_onload')
+  .getAttribute('data-client_id');
 
-async function google_login(response) {
+async function googleLogin(response) {
   let responsePayload;
   if (response.access_token) {
-    const url =
-      "https://www.googleapis.com/oauth2/v3/userinfo?access_token=" +
-      response.access_token;
+    const url = `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${
+      response.access_token}`;
     responsePayload = await fetch(url).then((res) => res.json());
   } else {
+    // eslint-disable-next-line no-undef
     responsePayload = jwt_decode(response.credential);
   }
-  const email = responsePayload.email;
-  localStorage.setItem("email", email);
-  window.location.href = "/market.html";
+  const { email } = responsePayload;
+  localStorage.setItem('email', email);
+  window.location.href = '/market.html';
 }
 
-var client = google.accounts.oauth2.initTokenClient({
-  client_id: client_id,
-  scope: "openid",
-  ux_mode: "popup",
-  response_type: "id_token token",
-  callback: google_login,
+// eslint-disable-next-line no-undef
+const client = google.accounts.oauth2.initTokenClient({
+  client_id: clientId,
+  scope: 'openid',
+  ux_mode: 'popup',
+  response_type: 'id_token token',
+  callback: googleLogin,
 });
 
 document
-  .querySelector("#login_with_google")
-  .addEventListener("click", function (e) {
+  .querySelector('#login_with_google')
+  .addEventListener('click', (e) => {
     client.requestAccessToken();
     e.preventDefault();
   });
--- js/products.js
+++ js/products.js
@@ -1,58 +1,202 @@
 const { useEffect, useState } = React;
 
-const Product = (props) => {
-  const { image, name, description, soldCount } = props.product;
+/**
+ * Product Component.
+ * @param {*} props
+ * @returns
+ */
+function Product(props) {
+  const { product } = props;
+  const {
+    image, name, description, soldCount, cost, currency,
+  } = product;
   return (
-    <div class="list-group-item list-group-item-action">
-      <div class="row">
-        <div class="col-3">
-          <img src={image} alt="product image" class="img-thumbnail"></img>
+    <div className="list-group-item list-group-item-action">
+      <div className="row">
+        <div className="col-3">
+          <img alt="product_image" src={image} className="img-thumbnail" />
         </div>
-        <div class="col">
-          <div class="d-flex w-100 justify-content-between">
-            <div class="mb-1">
-              <h4>{name}</h4>
+        <div className="col">
+          <div className="d-flex w-100 justify-content-between">
+            <div className="mb-1">
+              <h4>
+                {name}
+                {' '}
+                -
+                {currency}
+                {' '}
+                {cost}
+              </h4>
               <p>{description}</p>
             </div>
-            <small class="text-muted">{soldCount} vendidos</small>
+            <small className="text-muted">
+              {soldCount}
+              {' '}
+              vendidos
+            </small>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+Product.propTypes = {
+  product: PropTypes.element.isRequired,
 };
 
-const ReactApp = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const fetchData = async (category_id) => {
-      const url = `https://japceibal.github.io/emercado-api/cats_products/${category_id}.json`;
-      const result = await fetch(url)
-        .then((response) => response.json())
-        .catch((e) => null);
-      setData(result);
-    };
-    fetchData(101);
-  });
+class Sort extends React.Component {
+  constructor({ handleChange }) {
+    super();
+    this.state = '';
+    this.handleChange = handleChange;
+    this.data = [
+      {
+        id: 'sortAsc',
+        content: 'A-Z',
+        exec: (a, b) => a.name.localeCompare(b.name),
+      },
+      {
+        id: 'sortDesc',
+        content: 'Z-A',
+        exec: (a, b) => b.name.localeCompare(a.name),
+      },
+      {
+        id: 'sortByCount',
+        content: (<i className="fas fa-sort-amount-down mr-1" />),
+        exec: (a, b) => a.soldCount - b.soldCount,
+      },
+    ];
+  }
 
-  const render_products = () =>
-    data.products.map((product) => (
-      <Product key={product.id} product={product} />
-    ));
+  setValue(event) {
+    this.state = event.target.value;
+    this.handleChange(this.data.find((el) => el.id === this.state).exec);
+  }
 
-  return (
-    <div>
-      <div class="text-center p-4">
-        <h2>Productos</h2>
-        <p class="lead">
-          Verás aquí todos los productos de la categoría Autos.
-        </p>
+  render() {
+    return (
+      <div className="col text-end">
+        <div
+          onChange={this.setValue.bind(this)}
+          className="btn-group btn-group-toggle mb-4"
+          data-bs-toggle="buttons"
+        >
+          {this.data.map(({ id, content }) => (
+            <div>
+              <input
+                type="radio"
+                className="btn-check"
+                value={id}
+                name="options"
+                id={id}
+              />
+              <label className="btn btn-light" htmlFor={id}>
+                {content}
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
-      <div class="container">
-        <div id="product-list">{data && render_products()}</div>
+    );
+  }
+}
+
+Sort.propTypes = {
+  handleChange: PropTypes.element.isRequired,
+};
+
+function Filter() {
+  return (
+    <div className="col-lg-6 offset-lg-6 col-md-12 mb-1 container">
+      <div className="row container p-0 m-0">
+        <div className="col">
+          <p className="font-weight-normal text-end my-2">Cant.</p>
+        </div>
+        <div className="col">
+          <input
+            className="form-control"
+            type="number"
+            placeholder="min."
+            id="rangeFilterCountMin"
+          />
+        </div>
+        <div className="col">
+          <input
+            className="form-control"
+            type="number"
+            placeholder="máx."
+            id="rangeFilterCountMax"
+          />
+        </div>
+        <div className="col-3 p-0">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-light btn-block" id="rangeFilterCount">
+              Filtrar
+            </button>
+            <button type="button" className="btn btn-link btn-sm" id="clearRangeFilter">
+              Limpiar
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
-ReactDOM.render(<ReactApp />, document.getElementById("root"));
+function ReactApp() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async (categoryId) => {
+      const url = `https://japceibal.github.io/emercado-api/cats_products/${categoryId}.json`;
+      const res = await getJSONData(url);
+      const result = res.data;
+      if (result) {
+        setData(result);
+      }
+    };
+    const categoryId = localStorage.getItem('catID')
+      ? localStorage.getItem('catID')
+      : 101;
+    fetchData(categoryId);
+  }, []);
+
+  const handleChange = (sort) => {
+    if (data) {
+      data.products.sort(sort);
+      setData({ products: data.products });
+    }
+  };
+
+  const renderProducts = (products) => products.map((product) => (
+    <Product key={product.id} product={product} />
+  ));
+
+  if (data) {
+    return (
+      <div>
+        <div className="text-center p-4">
+          <h2>Productos</h2>
+          <p className="lead">
+            Verás aquí todos los productos de la categoría
+            {' '}
+            {data.catName}
+            .
+          </p>
+        </div>
+        <div className="container">
+          <div className="row">
+            <Sort handleChange={handleChange} />
+          </div>
+          <div className="row">
+            <Filter />
+          </div>
+          <div id="product-list">{renderProducts(data.products)}</div>
+        </div>
+      </div>
+    );
+  }
+  return <div />;
+}
+
+ReactDOM.render(<ReactApp />, document.getElementById('root'));
