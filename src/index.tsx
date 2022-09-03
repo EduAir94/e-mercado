fa4d075e388e3975003a0023caed6ada73b361ab
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2022-09-03 09:09:56 -0300

    fix: Hash router (to support ghpages)

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './store'
 import Spinner from './components/Spinner';
@@ -13,9 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   <Spinner/>
   </Provider>,
 );
